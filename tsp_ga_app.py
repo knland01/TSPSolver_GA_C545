e31b4d8ca30acda29cfacfdf255477115e6f03d1
wandercode <ohblisterdigits@gmail.com>
2024-10-17 15:45:58 -0400

    Is it the pop / max gen size?

--- tsp_ga_app.py
+++ tsp_ga_app.py
@@ -9,8 +9,8 @@ st.write("This app demonstrates the evolution of a solution to the Traveling Sal
 # Collect GA Parameters from User
 tsp_file = st.text_input("Enter TSP file path:", "Random100.tsp")
 data_set = st.selectbox("Select Data Set", ["D1_single_swap", "D2_single_invert", "D3_order_swap", "D4_order_invert"])
-pop_size = st.slider("Population Size", 50, 500, 200)
-max_gen = st.slider("Maximum Generations", 100, 1000, 200)
+pop_size = st.slider("Population Size", 5, 500, 5)
+max_gen = st.slider("Maximum Generations", 5, 500, 5)
 c_prob_high = st.slider("High Crossover Probability", 0.5, 1.0, 0.95)
 m_prob_high = st.slider("High Mutation Probability", 0.01, 0.1, 0.05)
 solution_type = st.selectbox("Solution Type", ["dict", "list"])
